98bd8d313ba72daddeb093236d435e85793f70b1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-03 21:01:50 +0300

    Finished
    Very simple project of how to fetch data from API and display it on the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+  const [catFact, setCatFact] = useState("")
+
+  const fetchCatFact = () => {
+    axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  }
+  useEffect(() => {
+    fetchCatFact()
+  }, [])
+
+
   return (
     <div className="App">
-     <button>Generate Cat Fact</button>
-     <p></p>
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
